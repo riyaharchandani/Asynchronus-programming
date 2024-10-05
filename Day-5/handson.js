0ce00421c96138e962641c2a0c27d0bc7b5179f2
riyaharchandani <riyaharchandani76@gmail.com>
2024-10-05 22:08:02 +0530

    Add initial files

--- Day-5/handson.js
+++ Day-5/handson.js
@@ -1,8 +1,22 @@
+function testScope() {
+    if (true) {
+      var varVariable = 'I am var';
+      let letVariable = 'I am let';
+      const constVariable = 'I am const';
+    }
+    console.log(varVariable);   
+    
+  }
+  
+  testScope();
+  
+
+//    task 2
 let nums = [10, 20, 30];
 
-nums.unshift(5);  // [5, 10, 20, 30]
-nums.push(40);    // [5, 10, 20, 30, 40]
-nums.splice(1, 1); // Removes element at index 1
+nums.unshift(5); 
+nums.push(40);    
+nums.splice(1, 1); 
 
 let total = nums.reduce((sum, num) => sum + num, 0);
-console.log(total); // Output: 105
+console.log(total); 
